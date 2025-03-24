51b7ca8bb3f8cbcdb60ddb5c3180824e3e2301ef
snaw0110 <96187496+snaw0110@users.noreply.github.com>
2025-03-24 17:59:22 +0900

    Update SmsService.java

--- learning/src/main/java/com/learning/app/user/SmsService.java
+++ learning/src/main/java/com/learning/app/user/SmsService.java
@@ -9,9 +9,9 @@ import net.nurigo.java_sdk.exceptions.CoolsmsException;
 
 public class SmsService {
 	// coolsms 내용 전달
-	private static final String API_KEY = "NCSZJ5VK7U34CXOW";
-	private static final String API_SECRET = "D3ASWMFORIMPGV4FNSMDFC65GQJOOBDD";
-	private static final String FROM_NUMBER = "01049396668";
+	private static final String API_KEY = "";
+	private static final String API_SECRET = "";
+	private static final String FROM_NUMBER = "";
 
 
 	public String sendVerificationSms(String to) throws CoolsmsException {
